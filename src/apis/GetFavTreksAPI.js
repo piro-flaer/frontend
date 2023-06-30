e2561497d2da84c321384ce99a95fe13cd50c843
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-06-30 11:34:13 +0530

    Create GetFavTreksAPI.js

---
+++ src/apis/GetFavTreksAPI.js
@@ -0,0 +1,11 @@
+const GetFavTreksAPI = async ({ userName }) => {
+  const url = new URL(
+    process.env.BACKEND_URL + "favorite?userName=" + userName
+  );
+
+  const response = await (await fetch(url)).json();
+
+  return response;
+};
+
+export default GetFavTreksAPI;

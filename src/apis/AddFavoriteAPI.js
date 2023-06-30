cbc1283a9f2e091df31ab198757fc9424c628296
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-06-30 11:33:07 +0530

    Create AddFavoriteAPI.js

---
+++ src/apis/AddFavoriteAPI.js
@@ -0,0 +1,18 @@
+const AddFavoriteAPI = async ({ userName, trekName }) => {
+  const url = new URL(process.env.BACKEND_URL + "favorite/add");
+
+  const response = await fetch(url, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userName: userName,
+      favoriteItem: trekName,
+    }),
+  });
+
+  const statusCode = await response.status;
+
+  return statusCode;
+};
+
+export default AddFavoriteAPI;

4c346b2c04a9277c9995a8639398e5b86c1f6e8e
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 17:22:38 +0530

    Update AllPages.jsx

--- src/pages/AllPages.jsx
+++ src/pages/AllPages.jsx
@@ -1,39 +1,42 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import TDPHeader from "./components/jsx/TDPHeader";
+import TDPBody from "./components/jsx/TDPBody";
+import { Button } from "@mui/material";
+import ForwardIcon from "@mui/icons-material/Forward";
 
-import LandingPage from "./landingPage/LandingPage";
-import LogInPage from "./loginSignupPage/LogInPage";
-import SignUpPage from "./loginSignupPage/SignUpPage";
-import PreferencePage from "./preferencesPage/PreferencePage";
-import HomePage from "./homePage/HomePage";
-import ProfilePage from "./profilePage/ProfilePage";
-import FavoritePage from "./profilePage/FavoritePage";
-import PreferencePageSideSection from "./profilePage/PreferencePageSideSection";
-import TrekDetailPage from "./trekDetailShower/TrekDetailPage";
-import CategoryShow from "./categoryShower/CategoryShow";
-
-const AllPages = () => {
+const TrekDetailPage = () => {
+  const location = useLocation();
+  const { trekArray } = location.state;
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LogInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/preferences" element={<PreferencePage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/my-profile" element={<ProfilePage />} />
-          <Route path="/my-favorites" element={<FavoritePage />} />
-          <Route
-            path="/my-preferences"
-            element={<PreferencePageSideSection />}
-          />
-          <Route path="/trek/:trekName" element={<TrekDetailPage />} />
-          <Route path="/category/:categoryName" element={<CategoryShow />} />
-        </Routes>
-      </BrowserRouter>
+      <Link to={"/home"} style={{ textDecoration: "none" }}>
+        <Button
+          style={{
+            position: "absolute",
+            top: "20px",
+            left: "20px",
+            zIndex: 1,
+            color: "white",
+          }}
+          variant="contained"
+          startIcon={<ForwardIcon sx={{ transform: "rotateZ(180deg)" }} />}
+        >
+          Go Home
+        </Button>
+      </Link>
+      <div className="trekDetailPage">
+        <TDPHeader imgSrc={trekArray.img} />
+        <TDPBody
+          trekName={trekArray.name}
+          trekState={trekArray.state}
+          trekSeason={trekArray.season}
+          trekDifficulty={trekArray.difficulty}
+          trekDescription={trekArray.description}
+        />
+      </div>
     </>
   );
 };
 
-export default AllPages;
+export default TrekDetailPage;

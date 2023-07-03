ed9b486c1ec1b14891d8166469fe707eee7c1356
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 12:46:03 +0530

    Update GetPreferencesAPI.js

--- src/apis/GetPreferencesAPI.js
+++ src/apis/GetPreferencesAPI.js
@@ -1,9 +1,9 @@
 const GetPreferencesAPI = async ({ userName }) => {
   const url = new URL(
-    process.env.BACKEND_URL + "preferences?userName=" + userName
+    process.env.REACT_APP_BACKEND_URL + "preferences?userName=" + userName
   );
-
   const response = await (await fetch(url)).json();
+  console.log("hey", response);
 
   return response;
 };

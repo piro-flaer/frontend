5d709f539f501c7bb08c0b2d0ec1894462c83eb3
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 13:35:09 +0530

    Update SignUpPage.jsx

--- src/pages/loginSignupPage/SignUpPage.jsx
+++ src/pages/loginSignupPage/SignUpPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import LogInPageBackground from "./components/jsx/LogInPageBackground"
-import SignUpPageDesign from "./components/jsx/SignUpPageDesign"
+import React from "react";
+import LogInPageBackground from "./components/jsx/LogInPageBackground";
+import SignUpPageDesign from "./components/jsx/SignUpPageDesign";
 
 const SignUpPage = () => {
-    return (
-        <>
-            <LogInPageBackground />
-            <SignUpPageDesign />
-        </>
-    )
-}
+  return (
+    <>
+      <LogInPageBackground />
+      <SignUpPageDesign />
+    </>
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;

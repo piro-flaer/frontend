fbc1f5a431a2bb77ab5c5af80172474627313bd5
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 13:45:30 +0530

    Update PreferenceFormPart.jsx

--- src/pages/preferencesPage/components/jsx/PreferenceFormPart.jsx
+++ src/pages/preferencesPage/components/jsx/PreferenceFormPart.jsx
@@ -1,56 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import PrefFormQuestion from "./PrefFormQuestion"
-import "../css/PreferenceFormPart.css"
+import PrefFormQuestion from "./PrefFormQuestion";
+import "../css/PreferenceFormPart.css";
 
-const PreferenceFormPart = ({ divIndexValue, directionValue, prefStateValue, prefSeasonValue, prefDifficultyValue }) => {
+const PreferenceFormPart = ({
+  divIndexValue,
+  directionValue,
+  prefStateValue,
+  prefSeasonValue,
+  prefDifficultyValue,
+}) => {
+  const variants = {
+    enter: (directionValue) => {
+      return {
+        zIndex: 0,
+        x: directionValue > 0 ? 750 : -750,
+        opacity: 0,
+      };
+    },
+    center: {
+      zIndex: 1,
+      x: 0,
+      opacity: 1,
+    },
+    exit: (directionValue) => {
+      return {
+        zIndex: 0,
+        x: directionValue < 0 ? 750 : -750,
+        opacity: 0,
+      };
+    },
+  };
 
-    const variants = {
-        enter: (directionValue) => {
-            return {
-                zIndex: 0,
-                x: directionValue > 0 ? 750 : -750,
-                opacity: 0
-            };
-        },
-        center: {
-            zIndex: 1,
-            x: 0,
-            opacity: 1
-        },
-        exit: (directionValue) => {
-            return {
-                zIndex: 0,
-                x: directionValue < 0 ? 750 : -750,
-                opacity: 0
-            };
-        }
-    };
+  return (
+    <div className="preferenceFormHolder">
+      <AnimatePresence initial={false} custom={directionValue}>
+        <motion.div
+          key={divIndexValue}
+          custom={directionValue}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.5 },
+          }}
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <PrefFormQuestion
+            divIndex={divIndexValue}
+            prefState={prefStateValue}
+            prefSeason={prefSeasonValue}
+            prefDifficulty={prefDifficultyValue}
+          />
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+};
 
-
-    return (
-        <div className="preferenceFormHolder">
-            <AnimatePresence initial={false} custom={directionValue}>
-                <motion.div
-                    key={divIndexValue}
-                    custom={directionValue}
-                    variants={variants}
-                    initial="enter"
-                    animate="center"
-                    exit="exit"
-                    transition={{
-                        x: { type: "spring", stiffness: 300, damping: 30 },
-                        opacity: { duration: 0.5 }
-                    }}
-                    style={{
-                        position: "absolute", width: "100%", height: "100%"
-                    }}
-                >
-                    <PrefFormQuestion divIndex={divIndexValue} prefState={prefStateValue} prefSeason={prefSeasonValue} prefDifficulty={prefDifficultyValue} />
-                </motion.div>
-            </AnimatePresence >
-        </div>
-    )
-}
-
-export default PreferenceFormPart
+export default PreferenceFormPart;

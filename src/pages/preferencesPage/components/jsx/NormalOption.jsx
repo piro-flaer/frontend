5b127cee9e1111b7c766283ba5e34dbb82bc834d
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 13:44:11 +0530

    Update NormalOption.jsx

--- src/pages/preferencesPage/components/jsx/NormalOption.jsx
+++ src/pages/preferencesPage/components/jsx/NormalOption.jsx
@@ -1,25 +1,74 @@
-import React from 'react';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import React from "react";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-const NormalOption = ({ divIndexValue, prefStateValue, prefSeasonValue, prefDifficultyValue, optionValue }) => {
+const NormalOption = ({
+  divIndexValue,
+  prefStateValue,
+  prefSeasonValue,
+  prefDifficultyValue,
+  optionValue,
+}) => {
+  return (
+    <>
+      {divIndexValue === 0 ? (
+        <span
+          className={
+            optionValue === prefStateValue
+              ? "optionSpan selectedOptionSpan"
+              : "optionSpan"
+          }
+        >
+          {optionValue}
+        </span>
+      ) : divIndexValue === 1 ? (
+        <span
+          className={
+            optionValue === prefSeasonValue
+              ? "optionSpan selectedOptionSpan"
+              : "optionSpan"
+          }
+        >
+          {optionValue}
+        </span>
+      ) : (
+        <span
+          className={
+            optionValue === prefDifficultyValue
+              ? "optionSpan selectedOptionSpan"
+              : "optionSpan"
+          }
+        >
+          {optionValue}
+        </span>
+      )}
 
+      {divIndexValue === 0 ? (
+        <CheckCircleIcon
+          className={
+            optionValue === prefStateValue
+              ? "checkIcon showCheckIcon"
+              : "checkIcon"
+          }
+        />
+      ) : divIndexValue === 1 ? (
+        <CheckCircleIcon
+          className={
+            optionValue === prefSeasonValue
+              ? "checkIcon showCheckIcon"
+              : "checkIcon"
+          }
+        />
+      ) : (
+        <CheckCircleIcon
+          className={
+            optionValue === prefDifficultyValue
+              ? "checkIcon showCheckIcon"
+              : "checkIcon"
+          }
+        />
+      )}
+    </>
+  );
+};
 
-
-    return (
-        <>
-            {
-                divIndexValue === 0 ? <span className={optionValue === prefStateValue ? "optionSpan selectedOptionSpan" : "optionSpan"}>{optionValue}</span> :
-                    divIndexValue === 1 ? <span className={optionValue === prefSeasonValue ? "optionSpan selectedOptionSpan" : "optionSpan"}>{optionValue}</span> :
-                        <span className={optionValue === prefDifficultyValue ? "optionSpan selectedOptionSpan" : "optionSpan"}>{optionValue}</span>
-            }
-
-            {
-                divIndexValue === 0 ? <CheckCircleIcon className={optionValue === prefStateValue ? "checkIcon showCheckIcon" : "checkIcon"} /> :
-                    divIndexValue === 1 ? <CheckCircleIcon className={optionValue === prefSeasonValue ? "checkIcon showCheckIcon" : "checkIcon"} /> :
-                        <CheckCircleIcon className={optionValue === prefDifficultyValue ? "checkIcon showCheckIcon" : "checkIcon"} />
-            }
-        </>
-    )
-}
-
-export default NormalOption
+export default NormalOption;

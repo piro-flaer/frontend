010bd6a9d0c4e6b80eb0d3865035ff086c530f4c
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 12:56:30 +0530

    Update HomePage.jsx

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import Header from './components/jsx/Header';
-import MainDiv from './components/jsx/MainDiv';
-import RestBody from './components/jsx/RestBody';
+import React, { useState, useEffect } from "react";
+import Header from "./components/jsx/Header";
+import MainDiv from "./components/jsx/MainDiv";
+import RestBody from "./components/jsx/RestBody";
+import TrekListAPI from "../../apis/TrekListAPI";
 
 const HomePage = () => {
-    return (
-        <>
-            <Header />
-            <MainDiv />
-            <div style={{ position: 'absolute', top: "90vh" }}>
-                <RestBody />
-            </div>
-        </>
-    )
-}
+  const [Treks, setTreks] = useState();
 
-export default HomePage
+  const generateArray = async () => {
+    const response = await TrekListAPI({});
+    setTreks(response);
+  };
+  useEffect(() => {
+    generateArray();
+  }, []);
+  return (
+    <>
+      <Header />
+      {Treks && <MainDiv Treks={Treks} />}
+      <div style={{ position: "absolute", top: "90vh" }}>
+        <RestBody />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;

1fbe4a07180a55041aec136c12ab9b40762f699d
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 12:49:41 +0530

    Update SignUpAPI.js

--- src/apis/SignUpAPI.js
+++ src/apis/SignUpAPI.js
@@ -1,19 +1,30 @@
-const SignUpAPI = async ({ userName, password }) => {
-  const url = new URL("http://localhost:3500/auth");
+const SignUpAPI = async ({
+  firstName,
+  lastName,
+  email,
+  userName,
+  password,
+  profile,
+}) => {
+  const url = new URL(process.env.REACT_APP_BACKEND_URL + "profile");
 
   const response = await fetch(url, {
     method: "post",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
       userName: userName,
       password: password,
+      profile: profile,
     }),
   });
 
-  const result = await response.json();
-  console.log(result);
+  const message = await response.json();
+  const statusCode = await response.status;
 
-  return result;
+  return { message, statusCode };
 };
 
 export default SignUpAPI;

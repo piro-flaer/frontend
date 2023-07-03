d1fa790fd7f7a206193c754b7fbf908daebb8402
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 17:21:35 +0530

    Update PreferenceSelector.jsx

--- src/pages/profilePage/components/jsx/PreferenceSelector.jsx
+++ src/pages/profilePage/components/jsx/PreferenceSelector.jsx
@@ -1,34 +1,42 @@
-import React from 'react';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import ListItemText from '@mui/material/ListItemText';
-import Select from '@mui/material/Select';
-import Checkbox from '@mui/material/Checkbox';
+import React from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
 
-const PreferenceSelector = ({ tag, valueArray, setValue, preferredValue }) => {
+const PreferenceSelector = ({
+  tag,
+  valueArray,
+  setValue,
+  preferredValue,
+  setDisable,
+}) => {
+  return (
+    <>
+      <FormControl sx={{ width: 300, m: 2.5 }}>
+        <InputLabel>{tag}</InputLabel>
+        <Select
+          value={preferredValue}
+          onChange={(event) => {
+            setValue(event.target.value);
+            setDisable(false);
+          }}
+          input={<OutlinedInput label={tag} />}
+          renderValue={(selected) => selected}
+        >
+          {valueArray.map((value) => (
+            <MenuItem key={value} value={value}>
+              <Checkbox checked={preferredValue === value} />
+              <ListItemText>{value}</ListItemText>
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </>
+  );
+};
 
-    return (
-        <>
-            <FormControl sx={{ width: 300, m: 2.5 }}>
-                <InputLabel>{tag}</InputLabel>
-                <Select
-                    value={preferredValue}
-                    onChange={(event) => { setValue(event.target.value) }}
-                    input={<OutlinedInput label={tag} />}
-                    renderValue={(selected) => selected}
-                >
-                    {valueArray.map((value) => (
-                        <MenuItem key={value} value={value}>
-                            <Checkbox checked={preferredValue === value} />
-                            <ListItemText>{value}</ListItemText>
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-        </>
-    );
-}
-
-export default PreferenceSelector
+export default PreferenceSelector;

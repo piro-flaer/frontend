11766d4a345e43aecf3cd3a152c4415ed744f943
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-23 13:13:34 +0530

    Update PreferencePage.jsx

--- src/pages/preferencesPage/PreferencePage.jsx
+++ src/pages/preferencesPage/PreferencePage.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import LogInPageBackground from "../loginSignupPage/components/jsx/LogInPageBackground";
 import PreferencePageDesign from "./components/jsx/PreferencePageDesign";
-import { useLocation } from "react-router-dom";
 
 const PreferencePage = () => {
-  const location = useLocation();
-  const { userName } = location.state;
   return (
     <>
       <LogInPageBackground />
-      <PreferencePageDesign userName={userName} />
+      <PreferencePageDesign />
     </>
   );
 };

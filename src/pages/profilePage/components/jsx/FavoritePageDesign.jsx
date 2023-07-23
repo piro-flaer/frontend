b0657b9d8e8ef36a84e70ca19f58ab857c6806d4
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-23 13:15:54 +0530

    Update FavoritePageDesign.jsx

--- src/pages/profilePage/components/jsx/FavoritePageDesign.jsx
+++ src/pages/profilePage/components/jsx/FavoritePageDesign.jsx
@@ -1,108 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import "../css/FavoritePageDesign.css";
 import GetFavTreksAPI from "../../../../apis/GetFavTreksAPI";
+import { Link } from "react-router-dom";
 
 const FavoritePageDesign = () => {
-  const userName = localStorage.getItem("userName");
-  var userFavoriteTreks = [];
+  const [userFavoriteTreks, setUserFavoriteTreks] = useState([]);
+
   const generateArray = async () => {
-    userFavoriteTreks = await GetFavTreksAPI({ userName });
+    const apiResponse = await GetFavTreksAPI();
+    setUserFavoriteTreks(apiResponse);
   };
-  generateArray();
+
+  useEffect(() => {
+    generateArray();
+  }, []);
   return (
     <>
       <div className="sectionProfileHolder favoriteSection">
         <div className="favoriteHeader">
           Here are your Favorites <FavoriteIcon sx={{ color: "red" }} /> :
         </div>
-        <div className="favoriteBody">
-          {userFavoriteTreks?.map((trek, index) => {
-            return (
-              <motion.div
-                key={index}
-                className="sectionBodyItem favoriteSecItem"
-                onHoverStart={() => {
-                  document.querySelector(
-                    "#" + "section_" + index + "_trekHolder"
-                  ).style.transitionDelay = "0s";
-                  document.querySelector(
-                    "#" + "section_" + index + "_img"
-                  ).style.transitionDelay = "0s";
-                  document.querySelector(
-                    "#" + "section_" + index + "_p"
-                  ).style.transitionDelay = "0s";
-                  document
-                    .querySelector("#" + "section_" + index + "_trekHolder")
-                    .classList.add("trekHolderHover");
-                  document
-                    .querySelector("#" + "section_" + index + "_img")
-                    .classList.add("trekHolderHoverImgP");
-                  document
-                    .querySelector("#" + "section_" + index + "_p")
-                    .classList.add("trekHolderHoverImgP");
-                  document.querySelector(
-                    "#" + "section_" + index + "_sectionBodyExpand"
-                  ).style.transitionDelay = "0.5s";
-                  document
-                    .querySelector(
-                      "#" + "section_" + index + "_sectionBodyExpand"
-                    )
-                    .classList.add("open");
-                }}
-                onHoverEnd={() => {
-                  document.querySelector(
-                    "#" + "section_" + index + "_trekHolder"
-                  ).style.transitionDelay = "0.5s";
-                  document.querySelector(
-                    "#" + "section_" + index + "_img"
-                  ).style.transitionDelay = "0.5s";
-                  document.querySelector(
-                    "#" + "section_" + index + "_p"
-                  ).style.transitionDelay = "0.5s";
-                  document
-                    .querySelector("#" + "section_" + index + "_trekHolder")
-                    .classList.remove("trekHolderHover");
-                  document
-                    .querySelector("#" + "section_" + index + "_img")
-                    .classList.remove("trekHolderHoverImgP");
-                  document
-                    .querySelector("#" + "section_" + index + "_p")
-                    .classList.remove("trekHolderHoverImgP");
-                  document.querySelector(
-                    "#" + "section_" + index + "_sectionBodyExpand"
-                  ).style.transitionDelay = "0s";
-                  document
-                    .querySelector(
-                      "#" + "section_" + index + "_sectionBodyExpand"
-                    )
-                    .classList.remove("open");
-                }}
-                whileTap={{
-                  scale: 0.9,
-                }}
-                whileHover={{
-                  zIndex: 1,
-                }}
-              >
-                <div
-                  id={"section_" + index + "_trekHolder"}
-                  className="trekHolder large sectionImg"
+        {userFavoriteTreks && (
+          <div className="favoriteBody">
+            {userFavoriteTreks.map((trek, index) => {
+              return (
+                <Link
+                  key={index}
+                  to={"/trek/" + trek.name}
+                  state={{ trekArray: trek }}
+                  style={{ textDecoration: "none" }}
                 >
-                  <img id={"section_" + index + "_img"} src={trek.img} />
-                  <p id={"section_" + index + "_p"}>{trek.name}</p>
-                </div>
-                <div
-                  id={"section_" + index + "_sectionBodyExpand"}
-                  className="sectionBodyExpand"
-                >
-                  {trek.description}
-                </div>
-              </motion.div>
-            );
-          })}
-        </div>
+                  <motion.div
+                    key={index}
+                    className="sectionBodyItem favoriteSecItem"
+                    onHoverStart={() => {
+                      document.querySelector(
+                        "#" + "section_" + index + "_trekHolder"
+                      ).style.transitionDelay = "0s";
+                      document.querySelector(
+                        "#" + "section_" + index + "_img"
+                      ).style.transitionDelay = "0s";
+                      document.querySelector(
+                        "#" + "section_" + index + "_p"
+                      ).style.transitionDelay = "0s";
+                      document
+                        .querySelector("#" + "section_" + index + "_trekHolder")
+                        .classList.add("trekHolderHover");
+                      document
+                        .querySelector("#" + "section_" + index + "_img")
+                        .classList.add("trekHolderHoverImgP");
+                      document
+                        .querySelector("#" + "section_" + index + "_p")
+                        .classList.add("trekHolderHoverImgP");
+                      document.querySelector(
+                        "#" + "section_" + index + "_sectionBodyExpand"
+                      ).style.transitionDelay = "0.5s";
+                      document
+                        .querySelector(
+                          "#" + "section_" + index + "_sectionBodyExpand"
+                        )
+                        .classList.add("open");
+                    }}
+                    onHoverEnd={() => {
+                      document.querySelector(
+                        "#" + "section_" + index + "_trekHolder"
+                      ).style.transitionDelay = "0.5s";
+                      document.querySelector(
+                        "#" + "section_" + index + "_img"
+                      ).style.transitionDelay = "0.5s";
+                      document.querySelector(
+                        "#" + "section_" + index + "_p"
+                      ).style.transitionDelay = "0.5s";
+                      document
+                        .querySelector("#" + "section_" + index + "_trekHolder")
+                        .classList.remove("trekHolderHover");
+                      document
+                        .querySelector("#" + "section_" + index + "_img")
+                        .classList.remove("trekHolderHoverImgP");
+                      document
+                        .querySelector("#" + "section_" + index + "_p")
+                        .classList.remove("trekHolderHoverImgP");
+                      document.querySelector(
+                        "#" + "section_" + index + "_sectionBodyExpand"
+                      ).style.transitionDelay = "0s";
+                      document
+                        .querySelector(
+                          "#" + "section_" + index + "_sectionBodyExpand"
+                        )
+                        .classList.remove("open");
+                    }}
+                    whileTap={{
+                      scale: 0.9,
+                    }}
+                    whileHover={{
+                      zIndex: 1,
+                    }}
+                  >
+                    <div
+                      id={"section_" + index + "_trekHolder"}
+                      className="trekHolder large sectionImg"
+                    >
+                      <img id={"section_" + index + "_img"} src={trek.img} />
+                      <p id={"section_" + index + "_p"}>{trek.name}</p>
+                    </div>
+                    <div
+                      id={"section_" + index + "_sectionBodyExpand"}
+                      className="sectionBodyExpand"
+                    >
+                      {trek.description}
+                    </div>
+                  </motion.div>
+                </Link>
+              );
+            })}
+          </div>
+        )}
       </div>
     </>
   );

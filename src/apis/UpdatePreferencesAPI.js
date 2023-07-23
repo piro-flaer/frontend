c4703d809ec477c5ef786a29ca381dc354c13c3b
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-23 11:48:09 +0530

    Update UpdatePreferencesAPI.js

--- src/apis/UpdatePreferencesAPI.js
+++ src/apis/UpdatePreferencesAPI.js
@@ -1,21 +1,21 @@
-const UpdatePreferencesAPI = async ({
-  userName,
-  state,
-  season,
-  difficulty,
-}) => {
-  const url = new URL(process.env.REACT_APP_BACKEND_URL + "preferences");
+const UpdatePreferencesAPI = async ({ state, season, difficulty }) => {
+  const accessToken = localStorage.getItem("accessToken");
 
-  const response = await fetch(url, {
+  const url = new URL(process.env.REACT_APP_BACKEND_URL + "preferences");
+  const options = {
     method: "put",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
-      userName: userName,
       state: state,
       season: season,
       difficulty: difficulty,
     }),
-  });
+  };
+
+  const response = await fetch(url, options);
 
   const statusCode = response.status;
 

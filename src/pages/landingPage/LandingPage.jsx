6c2ac16e3628f7450a592637027f7807958c0a0c
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-23 12:54:13 +0530

    Update LandingPage.jsx

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import jwtDecode from "jwt-decode";
+import RefreshAPI from "../../apis/RefreshAPI";
+import React, { useState, useEffect } from "react";
 import IntroSection from "./components/jsx/IntroSection";
+import { Navigate } from "react-router-dom";
 
 const LandingPage = () => {
-  return <IntroSection />;
+  const [Redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      setRedirect(true);
+      const decodedToken = jwtDecode(accessToken);
+      const expirationTime = decodedToken.exp;
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (expirationTime < currentTime) {
+        const apiResponse = RefreshAPI();
+        if (apiResponse === 401) {
+          setRedirect(false);
+        }
+      }
+    }
+  }, []);
+
+  return (
+    <>
+      {Redirect && <Navigate to="/home" />}
+      <IntroSection />
+    </>
+  );
 };
 
 export default LandingPage;
